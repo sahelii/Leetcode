512411b30727f67ab8567fa03947b56254953435
sahelii <saheliofficial22@gmail.com>
2023-08-05 00:51:06 +0530

    118.pascals-triangle

--- 118.pascals-triangle.java
+++ 118.pascals-triangle.java
@@ -7,8 +7,28 @@
 // @lc code=start
 class Solution {
     public List<List<Integer>> generate(int numRows) {
-        
+        List<List<Integer>> triangle = new ArrayList<>();
+
+        if (numRows == 0) {
+            return triangle;
+        }
+
+        List<Integer> firstRow = new ArrayList<>();
+        firstRow.add(1);
+        triangle.add(firstRow);
+
+        for (int i = 1; i < numRows; i++) {
+            List<Integer> prevRow = triangle.get(i - 1);
+            List<Integer> currRow = new ArrayList<>();
+
+            currRow.add(1);
+            for (int j = 1; j < i; j++) {
+                currRow.add(prevRow.get(j) + prevRow.get(j - 1));
+            }
+            currRow.add(1);
+            triangle.add(currRow);
+        }
+        return triangle;
     }
 }
 // @lc code=end
-
