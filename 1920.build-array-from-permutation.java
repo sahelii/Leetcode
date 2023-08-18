dea11f53e96fcb87bc93f401b8c5b69bf58e7936
sahelii <saheliofficial22@gmail.com>
2023-08-18 12:04:14 +0530

    1920.build-array-from-permutation

---
+++ 1920.build-array-from-permutation.java
@@ -0,0 +1,19 @@
+/*
+ * @lc app=leetcode id=1920 lang=java
+ *
+ * [1920] Build Array from Permutation
+ */
+
+// @lc code=start
+class Solution {
+    public int[] buildArray(int[] nums) {
+        int n = nums.length;
+        int[] ans = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            ans[i] = nums[nums[i]];
+        }
+        return ans;
+    }
+}
+// @lc code=end
